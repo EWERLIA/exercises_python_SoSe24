4798ebfe6c7ee77d6c769c20a64ddc28d45315db
EWERLIA <anton.werlitz@gmail.com>
2024-05-17 14:29:44 +0200

    .\exercise08.py

---
+++ exercise08.py
@@ -0,0 +1,12 @@
+def vokon_zählen(wort):
+    string=wort.lower()
+    vokale = 0
+    konsonanten = 0   
+    for i in string:
+        if i in ["a","e","i","o","u","ä","ü","ö"] and i.isalpha():
+            vokale += 1
+        elif i.isalpha() and i not in ["a","e","i","o","u","ö","ä","ü"]:
+            konsonanten +=1
+    print("Es gibt "+str( konsonanten )+" Konsonanten und "+str( vokale)+" Vokale.")
+  
+print(vokon_zählen("sfhhioqwdnqomadoscpakü"))
